2053468d46be22d0de445d10b66be9aa0abfdfd8
Claire W <cdwang@bu.edu>
2016-03-22 18:10:06 -0400

    changed url

--- popup.js
+++ popup.js
@@ -5,23 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
       
       checkPageButton.addEventListener('click', function() {
   
-      
+      chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
       var request = $.ajax( {
         url:'https://api.aylien.com/api/v1/summarize',
         //headers: { 'X-TextRazor-Key': '1e858ee751d3f99127f9ab1429176f82c7a944ff5f9c5d34ff906ceb'},
         headers:{'X-AYLIEN-TextAPI-Application-Key':'8968137332783762d4117e8532aff787','X-AYLIEN-TextAPI-Application-ID':'ac9f0736'},
         dataType: 'json',
         type: 'POST',
-        data: {url: 'http://www.bbc.com/news/world-latin-america-35867590'}
+        data: {url: url, sentences_number:2,sentences_percentage:80}
       
       });
 
       request.done(function(res) {
          console.log(res); 
-        document.getElementById("summary").innerHTML = res.text; 
+        document.getElementById("summary").innerHTML = res.sentences[0] + res.sentences[1]; 
 
       
       }); 
+    });
   
   }, true);
 }, true);
